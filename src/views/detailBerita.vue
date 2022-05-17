1500036c7c4be4bc9a210456d7d49df27c472e6a
raja <rajaenda521@gmail.com>
2022-05-17 21:27:13 +0700

    Add

--- src/views/detailBerita.vue
+++ src/views/detailBerita.vue
@@ -15,13 +15,22 @@
       <!-- Result -->
       
       <div v-else-if="data" class="result apollo">
-        <div style="margin-top: 80px; margin-bottom: 50px;">
-          <h1 style="margin-bottom: 20px;">{{ data.News_berita_by_pk.judul }}</h1>
-          <img style="border-radius:10px;" :src="data.News_berita_by_pk.image_path" />
-          <div style="background-color:white; height: 300px; border-radius:10px;">
-            <h4>Disukai oleh {{ data.News_berita_by_pk.like }} orang</h4>
-            <p>{{data.News_berita_by_pk.deskripsi}}</p>
-          </div>
+        <div style="margin-top: 100px; margin-bottom: 50px;">
+          <h1 style="margin-bottom: 30px;">{{ data.News_berita_by_pk.judul }}</h1>
+              <v-row>
+                
+                <v-col cols="4">
+                  <img style="border-radius:10px; width: 400px; background-color: white;" :src="data.News_berita_by_pk.image_path" />
+            </v-col>
+            <v-col cols="1"></v-col>
+            <v-col cols="5" style="background-color:white; border-radius: 15px;">
+              <h3>{{data.News_berita_by_pk.kategori}}</h3>              
+              <p>{{data.News_berita_by_pk.deskripsi}}</p>
+              <h5>Disukai oleh {{ data.News_berita_by_pk.like }} orang</h5>
+          
+            </v-col>
+          
+              </v-row>
         </div>
         <v-btn @click=" ()=> redirectTo('/list')" style="background-color:#98FB98; padding: 15px; margin-left: 400px; margin-right: auto;">Kembali</v-btn>
       </div>
